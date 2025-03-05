8ac826d81fac5400ad3238a1cb7c65eded6c85a8
Maria Gracy de Guzman <medeguzma4@up.edu.ph>
2025-03-05 12:47:06 +0800

    Updating files

--- request.js
+++ request.js
@@ -1,5 +1,12 @@
 import needle from 'needle';
 
-needle.get('http://localhost:3000/', (err, res) => {
+needle.get('http://localhost:3000/find-by-isbn-author?isbn=978-0-7475-3269-9&amp;author=J.K+Rowling', (err, res) => {
     console.log(res.body);   // prints the body of the response message. In this case, “Hello”
 });
+needle.post(
+    'http://localhost:3000/add-book',
+    { name: 'book-server' }, // can be an object or a string
+    (err, res) => {
+        console.log(res.body);
+    }
+);
--- server.js
+++ server.js
@@ -1,25 +1,71 @@
-const express = require('express');
-const fs = require('fs');
+import express from 'express';
+import fs from 'fs';
 const app = express();
-const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.post('/add-book', (req, res) => {
     const { bookName, isbn, author, yearPublished } = req.body;
     if (!bookName || !isbn || !author || !yearPublished) {
-        console.log('> {success: true}');
+        console.log('> {success: false}');
     } else {
-        return res.json; // sends a response
+        return res.json({ success: true }); // sends a response
     }
 
-    // if book is present
+    // check if there are current isbn
+    if (fs.existsSync('books.txt')) {
+        const books = fs.readFileSync('books.txt', 'utf8');
+        const bookCollection = books.trim().split('\n');
 
+        if (bookCollection.some(line => line.includes(isbn))) {
+            return res.json({ success: false });
+        }
+    }
+
+    // Add the book
+    fs.appendFileSync('books.txt', `${bookName},${isbn},${author},${yearPublished}\n`);
+    return res.send({ success: true })
 
 });
 
 app.get('/find-by-isbn-author', (req, res) => {
-    const {isbn, author} = req.query;
+    const { isbn, author } = req.query;
 
+    fs.readFile('books.txt', 'utf8', (err, data) => {
+        if (err) return res.json({ success: false })
 
+        const bookCollection = data.trim().split('\n');
+        const bookFind = bookCollection.find(line => {
+            const [curName, curIsbn, curAuthor, curYear] = line.split(',');
+            return curIsbn === isbn && curAuthor === author;
+        })
+
+        if (bookFind) {
+            const [curName, curIsbn, curAuthor, curYear] = bookCollection.split(',');
+            return res.json({ success: true }, { book: bookName, isbn: curIsbn, author: curAuthor, yearPublished });
+        } else {
+            return res.json({ success: false });
+        }
+    });
+});
+
+app.get('/find-by-author', (req, res) => {
+    const { author } = req.query;
+
+    fs.readFile('books.txt', 'utf8', (err, data) => {
+        if (err) return res.json({ success: false });
+
+        const bookCollection = data.trim().split('\n');
+        const bookFind = books.find(line => {
+            const [curName, curIsbn, curAuthor, curYear] = line.split(',');
+            return curAuthor === author;
+        })
+
+        if (bookFind) {
+            const [curName, curIsbn, curAuthor, curYear] = bookCollection.split(',');
+            return { bookName, isbn, author: curAuthor, yearPublished };
+        }
+    });
 });
 
 // this tells our server to listen to the port 3000
